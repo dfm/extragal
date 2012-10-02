22eeef72736b9501f07019b19ee425b6dcca4066
Dan F-M <danfm@nyu.edu>
2012-10-01 22:12:30 -0400

    problem 2

--- ps2/ps2.py
+++ ps2/ps2.py
@@ -2,9 +2,19 @@
 
 from __future__ import print_function
 
+import os
+
 import numpy as np
+from scipy.integrate import simps
 import matplotlib.pyplot as pl
+from matplotlib.ticker import MaxNLocator
 import requests
+
+
+try:
+    os.makedirs("results")
+except os.error:
+    pass
 
 
 sdss_url = "http://api.sdss3.org"
@@ -23,7 +33,7 @@ def find_star(ra, dec, radius):
 
     # Get the contents of the response.
     data = r.json
-    assert len(data) == 1
+    assert len(data) >= 1
 
     return data[0]
 
@@ -34,7 +44,7 @@ def get_star(ra, dec, radius=0.005):
 
     # Build request.
     url = sdss_url + "/spectrum"
-    pars = {"id": sdss_id, "format": "json", "fields": "flux,wavelengths"}
+    pars = {"id": sdss_id, "format": "json", }
 
     # Submit request.
     r = requests.get(url, params=pars)
@@ -43,16 +53,90 @@ def get_star(ra, dec, radius=0.005):
 
     # Get the contents of the response.
     data = r.json
-    assert len(data) == 1
+    assert len(data) >= 1
 
+    # Parse the results.
     data = data[0][sdss_id]
-
+    psfmag = data["psfmag"]
     wavelengths = np.array(data["wavelengths"])
     flux = np.array(data["flux"])
 
-    pl.plot(wavelengths, flux, "k")
-    pl.savefig(sdss_id + ".png")
+    return sdss_id, wavelengths, flux, psfmag
+
+
+def measure_hbeta(sdss_id, wavelengths, flux):
+    # H-beta regions.
+    region = [4847.875, 4876.625]
+    lower = [4827.875, 4847.875]
+    upper = [4876.625, 4891.625]
+
+    # Extract line.
+    inds = (wavelengths >= region[0]) * (wavelengths <= region[1])
+    x = wavelengths[inds]
+    y = flux[inds]
+
+    # Build the linear continuum.
+    il = (wavelengths >= lower[0]) * (wavelengths <= lower[1])
+    il_mid = np.argmin(np.abs(np.mean(lower) - wavelengths))
+    x1 = wavelengths[il_mid]
+    y1 = np.median(flux[il])
+
+    iu = (wavelengths >= upper[0]) * (wavelengths <= upper[1])
+    iu_mid = np.argmin(np.abs(np.mean(upper) - wavelengths))
+    x2 = wavelengths[iu_mid]
+    y2 = np.median(flux[iu])
+    continuum = (y2 - y1) * (x - x1) / (x2 - x1) + y1
+
+    # Integrate.
+    integrand = 1 - y / continuum
+    ew = simps(integrand, x=x)
+
+    # Plot some diagnostics.
+    pl.clf()
+
+    pl.plot(x, y, "k")
+    pl.plot(wavelengths[il], flux[il], "b")
+    pl.plot(wavelengths[iu], flux[iu], "b")
+    pl.plot(x, continuum, "--r")
+
+    pl.xlabel(r"$\lambda$")
+    pl.ylabel(r"$f$")
+    pl.title(r"$\mathrm{{H}}\beta = {0:.2f}$".format(ew))
+
+    pl.gca().xaxis.set_major_locator(MaxNLocator(6))
+    pl.gca().yaxis.set_major_locator(MaxNLocator(6))
+
+    pl.savefig("results/hbeta-{0}.pdf".format(sdss_id))
+
+    return ew
+
+
+def problem2():
+    pl.figure(figsize=[6, 6])
+
+    # Loop over the stars and save the equivalent widths.
+    stars = [[181.65434, 33.977438],
+             [180.92172, 25.519848],
+             [180.26576, 33.729540],
+             [178.76525, 31.807154],
+             [181.53276, 34.094667],
+             [177.30335, 30.758677],
+             [180.73127, 33.741772],
+             [174.63739, 30.887942]]
+
+    ews, colors = [], []
+    for ra, dec in stars:
+        sdss_id, wavelengths, flux, psfmag = get_star(ra, dec)
+        ew = measure_hbeta(sdss_id, wavelengths, flux)
+
+        ews.append(ew)
+        colors.append(psfmag["g"] - psfmag["r"])
+
+    # Plot the "relation".
+    pl.clf()
+    pl.plot(colors, ews, "ok")
+    pl.savefig("results/part2.pdf")
 
 
 if __name__ == "__main__":
-    get_star(181.65434, 33.977438)
+    problem2()
